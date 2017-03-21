69eb41f7319b44e73def980fd709b8611095e9ef
quyencao <boy20195@gmail.com>
2017-03-21 22:03:34 +0700

    Correcty

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://quyen:123456@ds137360.mlab.com:37360/todoapi' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
