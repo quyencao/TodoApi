e94018afced1392f5c3b221cf3905d4f094edf37
quyencao <boy20195@gmail.com>
2017-03-21 21:11:27 +0700

    Add test GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,18 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {ObjectID} = require('mongodb');
+
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [
     {
+        _id: new ObjectID(),
         text: "Clean the room"
     },
     {
+        _id: new ObjectID,
         text: "Walk dog"
     }
 ]
@@ -71,4 +75,34 @@ describe('GET /todos', () => {
            })
            .end(done);
     });
+});
+
+describe('GET /todos/:id', () => {
+   it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+   });
+
+   it('shound return 404 if todo not found', (done) => {
+        const _id = new ObjectID().toHexString();
+
+        request(app)
+            .get(`/todos/${_id}`)
+            .expect(404)
+            .end(done);
+   });
+
+   it('should return 404 for non-object ids', (done) => {
+        const _id = "123";
+
+        request(app)
+            .get(`/todos/${_id}`)
+            .expect(404)
+            .end(done);
+   });
 });
