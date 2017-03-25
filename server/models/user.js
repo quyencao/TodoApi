cbcfd9a682e4d4b66a2be5436032cb2c719d8f02
quyencao <boy20195@gmail.com>
2017-03-25 17:37:14 +0700

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+      $pull: {
+          tokens: { token }
+      }
+  });
+};
+
 // Model method
 UserSchema.statics.findByToken = function (token) {
   var User = this;
